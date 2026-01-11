4be50f72ca3918821b89b67ef49b8392a8a14c12
emmanuelselicour <emmanuelselicour.2002@gmail.com>
2026-01-10 21:05:45 -0500

    Delete middleware directory

--- middleware/auth.js
+++
@@ -1,9 +0,0 @@
-// Middleware pour vérifier l'authentification
-function authenticateToken(req, res, next) {
-    if (!req.session.user) {
-        return res.status(401).json({ error: 'Accès non autorisé. Veuillez vous connecter.' });
-    }
-    next();
-}
-
-module.exports = { authenticateToken };
